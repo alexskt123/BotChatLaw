682f1dbfe25bed7add51b02d27c7cad35a63a2fb
So Alex <as2120@ad01.bcthk.com>
2020-09-23 14:21:09 +0800

    support options

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 
 import styles from '../styles/Home.module.css'
 import ChatBot from 'react-simple-chatbot';
-import {Fragment} from 'react';
+import {Fragment , useEffect} from 'react';
 import Head from 'next/head';
 
 
 export default function Home() {
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <Fragment>
       <Head>
@@ -14,15 +19,33 @@ export default function Home() {
 
       </Head>      
       <div className={styles.container} >
-        <ChatBot
-              steps={[
-                {
-                  id: 'hello-world',
-                  message: 'Hello World!',
-                  end: true,
-                },
-              ]}
-            />         
+      <ChatBot
+        steps={[
+          {
+            id: '1',
+            message: 'What number I am thinking?',
+            trigger: '2',
+          },
+          {
+            id: '2',
+            options: [
+              { value: 1, label: 'Number 1', trigger: '4' },
+              { value: 2, label: 'Number 2', trigger: '3' },
+              { value: 3, label: 'Number 3', trigger: '3' },
+            ],
+          },
+          {
+            id: '3',
+            message: 'Wrong answer, try again.',
+            trigger: '2',
+          },
+          {
+            id: '4',
+            message: 'Awesome! You are a telepath!',
+            end: true,
+          },
+        ]}
+      />   
       </div>    
     
     </Fragment>
