e7a0848d89132852217f11a92d7290e8ee9cc4de
So Alex <as2120@ad01.bcthk.com>
2020-09-28 17:26:13 +0800

    dive 100h library

--- components/CustomChatBot.js
+++ components/CustomChatBot.js
@@ -2,6 +2,7 @@
 import ChatBot from 'react-simple-chatbot';
 import { Fragment } from 'react';
 import { ThemeProvider } from 'styled-components';
+import Div100vh from 'react-div-100vh';
 
 import React from 'react';
 
@@ -22,19 +23,20 @@ export default function CustomChatBot(inputSteps) {
     return (
         <Fragment>
             <ThemeProvider theme={theme}>
-                <ChatBot
-                    headerTitle="try try"
-                    placeholder="input ..."
-                    botAvatar="botAvatar.png"
-                    userAvatar="userAvatar.png"
-                    bubbleStyle={{ fontSize: '15px' }, { boxShadow: "1px 2px 5px #9E9E9E" }}
-                    bubbleOptionStyle={{ fontSize: '15px' }, { boxShadow: "1px 2px 5px #9E9E9E" }}
-                    width="100%"
-                    height="100vh"
-                    steps={
-                        inputSteps
-                    }
-                />
+                <Div100vh>
+                    <ChatBot
+                        headerTitle="法律小幫手"
+                        placeholder="請輸入。。。"
+                        botAvatar="botAvatar.png"
+                        userAvatar="userAvatar.png"
+                        bubbleStyle={{ fontSize: '15px' }, { boxShadow: "1px 2px 5px #9E9E9E" }}
+                        bubbleOptionStyle={{ fontSize: '15px' }, { boxShadow: "1px 2px 5px #9E9E9E" }}
+                        width="100%"
+                        steps={
+                            inputSteps
+                        }
+                    />
+                </Div100vh>
             </ThemeProvider>
 
 
