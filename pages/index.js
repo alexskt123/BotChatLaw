f345ba437d0d9055bf0198cd684dc068be3f73cd
condomlee <eddykidness@icloud.com>
2020-09-29 01:40:52 +0800

    example of custom hooks for realtime data

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
-// import from react
-import { useState, useEffect, Fragment, createElement } from 'react';
+//import from react
+import { Fragment, createElement } from 'react';
 //import hooks
 import { use100vh } from 'react-div-100vh';
+import { useSteps } from '../lib/hooks/useFire'
 //import components
 import CustomChatBot from '../components/CustomChatBot'
 import Others from '../components/Others'
 import StepMessage from '../components/StepMessage'
 import StepLink from '../components/StepLink'
-//import lib
-import { getSteps } from '../lib/firebaseResult';
 //export default component
 export default function CustomStep() {
   //varibles for component
@@ -18,22 +17,15 @@ export default function CustomStep() {
     "<StepLink/>": StepLink
   }
   //hooks
-  const [datas, setData] = useState([])
+  const steps = useSteps()
+  console.log({ steps })
   const height = use100vh()
-  //side effect
-  useEffect(() => {
-    //async side effect must use IIFE(Immediately Invoked Function Expression)
-    (async () => {
-      let fireStep = await getSteps()
-      setData(fireStep)
-    })()
-  }, [])
   //loading
-  if (datas.length <= 0) {
+  if (steps.length <= 0) {
     return <div>正在加載中....</div>
   }
   //processing
-  datas
+  steps
     .filter(data => data.component)
     .filter(data => typeof data.component === "string")
     .forEach(data => {
@@ -44,7 +36,7 @@ export default function CustomStep() {
   //template
   return (
     <Fragment>
-      <CustomChatBot steps={datas} height={`${height}px`} />
+      <CustomChatBot steps={steps} height={`${height}px`} />
     </Fragment>
   )
 }
