9466b8174fcde86afeaecc0dc05d34152a41592a
So Alex <as2120@ad01.bcthk.com>
2020-09-30 12:55:40 +0800

    more human

--- components/Others.js
+++ components/Others.js
@@ -1,15 +1,33 @@
 import { Fragment, useEffect, useState } from 'react'
 import { getIntentByQuery } from '../lib/getIntentByQuery'
 import Loading from './loading'
+import shuffle from 'shuffle-array'
 
 export default function Others({ previousStep, triggerNextStep }) {
   const [message, setMessage] = useState(null)
+
+  const guessMsg = [
+    '我估你嘅意思係:[label]',
+    '係咪[label]?',
+    '你係咪想搵[label]',
+    '[labe]? 輕鬆搵到'
+  ]
+
+  const notFoundMsg = [
+    '搵唔到....',
+    '你噏乜呀?',
+    '唔識...',
+    '再問遇啦...'
+  ]
+
+  shuffle(guessMsg)
+  shuffle(notFoundMsg)
 
   useEffect(() => {
     (async () => {
       const { doc } = await getIntentByQuery(previousStep.value)
 
-      let message = doc && doc.label ? `我估你嘅意思係: ${doc.label}` : '搵唔到....'
+      let message = doc && doc.label ? guessMsg[0].replace('[label]', `${doc.label}`) : notFoundMsg[0]
       setMessage(message)
 
       if (doc && doc.explanation) {
--- components/loading.js
+++ components/loading.js
@@ -1,9 +1,20 @@
 import { Fragment } from "react";
+import shuffle from 'shuffle-array'
 
 export default function Loading() {
+
+  const loadMsg = [
+    '幫緊你...',
+    '幫緊你幫緊你...',
+    '唔好急...',
+    '唔好急唔好急...'
+  ]
+
+  shuffle(loadMsg)
+
   return (
     <Fragment>
-      幫緊你幫緊你...
+      {loadMsg[0]}
     </Fragment>
   )
 }
