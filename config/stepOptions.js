89ef922032e4751136a0bafd4f8c5da33fbc1c22
So Alex <as2120@ad01.bcthk.com>
2020-10-08 14:21:01 +0800

    extract defaultOptions to config

--- config/stepOptions.js
+++ config/stepOptions.js
@@ -9,3 +9,28 @@ export const back = {
   value: 'head',
   trigger: 'head'
 }
+
+export const defaultOptions = [
+  {
+    label: '結束',
+    value: '結束',
+    trigger: 'tail'
+  },
+  {
+    label: '發問',
+    value: '發問',
+    trigger: 'other'
+  }
+  ,
+  {
+    label: '有咩可以問',
+    value: '有咩可以問',
+    trigger: 'stageintent'
+  }
+  ,
+  {
+    label: '聯絡我們',
+    value: '聯絡我們',
+    trigger: 'stagecontact'
+  }
+]
