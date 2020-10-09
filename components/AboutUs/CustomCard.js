b136886430b60ec3836d6cb3976e839aa32e0834
Li Eddy <el2083@ad01.bcthk.com>
2020-10-09 14:42:04 +0800

    hotfix margin

--- components/AboutUs/CustomCard.js
+++ components/AboutUs/CustomCard.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card'
 export default function CustomCard({ displayItems }) {
 
   return (
-    <Card border="dark" className="mx-auto" style={{ width: '18rem' }}>
+    <Card border="dark" className="mx-auto mx-sm-3" style={{ width: '18rem' }}>
       <Card.Img variant="top" src={displayItems.imgsrc} />
       <Card.Body>
         <Card.Title>{displayItems.title}</Card.Title>
