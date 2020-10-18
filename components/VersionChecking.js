efc48e5e060838f965c93159924dad6aaf6eb0dd
condomlee <eddykidness@icloud.com>
2020-10-18 16:02:27 +0800

    ask user if they want to update

--- components/VersionChecking.js
+++ components/VersionChecking.js
@@ -1,13 +1,19 @@
-import { useEffect } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { webConfig } from '../config/settings'
 import { useServerConfig } from '../lib/hooks/useFire'
 import semver from 'semver'
+import { Modal, Button } from 'react-bootstrap'
 
 export default function VersionChecking() {
   //local state for now, will move to global store
   const ServerConfig = useServerConfig()
   const router = useRouter()
+  const [updateAvail, setUpdateAvail] = useState(false)
+
+  const updateNow = () => {
+    router.reload()
+  }
 
   useEffect(() => {
     const localConfig = localStorage.getItem('web_config') || JSON.stringify(webConfig)
@@ -15,10 +21,12 @@ export default function VersionChecking() {
     const serverVersion = ServerConfig.webVersion || webConfig.webVersion
     const webVersion = JSON.parse(localConfig).webVersion
 
+    console.log({ serverVersion, webVersion })
+
     try {
       if (semver.lt(webVersion, serverVersion)) {
+        setUpdateAvail(true)
         localStorage.setItem('web_config', JSON.stringify(ServerConfig))
-        router.reload()
       }
     } catch (error) {
       console.error(error)
@@ -26,5 +34,27 @@ export default function VersionChecking() {
 
   }, [ServerConfig])
 
-  return ''
+  return updateAvail ? <UpdateModal show={updateAvail} onHide={() => setUpdateAvail(false)} update={() => updateNow()} /> : <Fragment />
+}
+
+export function UpdateModal({ show, onHide, update }) {
+  return (
+    <Modal show={show} onHide={onHide} size="lg" centered>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          {'Update Available!'}
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        {'Click "Update Now" to update.'}
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button onClick={update}>{'Update Now'}</Button>
+
+        <Button variant="secondary" onClick={onHide}>{'Later'}</Button>
+      </Modal.Footer>
+    </Modal>
+  )
 }
