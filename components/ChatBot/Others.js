5c44c0733321596214d6d653637ad626c28fa038
Li Eddy <el2083@ad01.bcthk.com>
2020-10-22 10:08:13 +0800

    since t may not be ready the first time, move logic into hook should fix the message problem

--- components/ChatBot/Others.js
+++ components/ChatBot/Others.js
@@ -17,15 +17,12 @@ function Others({ previousStep, triggerNextStep, t }) {
   const [notGuess, setNotGuess] = useState(null)
   const [clicked, setClicked] = useState(false)
 
-  const guessMessage = randomMsg(t('guessMsg', { returnObjects: true }))
-  const notFoundMessage = randomMsg(t('notFoundMsg', { returnObjects: true }))
-
-  const getMessage = (intentData, isNotGuess) => {
-    let message = notFoundMessage
-    const {doc} = intentData
+  const getMessage = (intentData, isNotGuess, messages) => {
+    let message = messages[1]
+    const { doc } = intentData
 
     if (doc && doc.label) {
-      message = (isNotGuess) ? doc.label : guessMessage.replace('[label]', `${doc.label}`)
+      message = (isNotGuess) ? doc.label : messages[0].replace('[label]', `${doc.label}`)
     }
 
     return message
@@ -34,21 +31,24 @@ function Others({ previousStep, triggerNextStep, t }) {
   useEffect(() => {
     (async () => {
 
-      let intentData = {...IntentData}      
+      let intentData = { ...IntentData }
 
       intentData = await getIntentByQuery(previousStep.value)
 
       let isNotGuess = notUseGuessMsg.find(x => x === intentData.intent) || (!(intentData.doc && intentData.doc.label))
 
-      let message = getMessage(intentData, isNotGuess)
+      const guessMessage = randomMsg(t('guessMsg', { returnObjects: true }))
+      const notFoundMessage = randomMsg(t('notFoundMsg', { returnObjects: true }))
+
+      let message = getMessage(intentData, isNotGuess, [guessMessage, notFoundMessage])
       setMessage(message)
 
-      await setRequestByInput(previousStep.value, intentData.intent)        
-      
+      await setRequestByInput(previousStep.value, intentData.intent)
+
       if (isNotGuess) {
-        triggerNextStep({trigger: intentData.trigger})
+        triggerNextStep({ trigger: intentData.trigger })
       }
-      
+
       setData(intentData)
       setNotGuess(isNotGuess)
 
@@ -59,9 +59,9 @@ function Others({ previousStep, triggerNextStep, t }) {
     setClicked(true)
 
     if (value) {
-      const intentData = {...data}
+      const intentData = { ...data }
       let trigger
-      let newTrigger 
+      let newTrigger
 
       if (value === 'yes') {
         if (intentData.doc && intentData.doc.explanations) {
@@ -70,11 +70,11 @@ function Others({ previousStep, triggerNextStep, t }) {
         else {
           trigger = intentData.trigger
         }
-        
+
         newTrigger = {
           trigger,
           value: intentData
-        }      
+        }
       }
       else {
         trigger = 'other'
@@ -88,7 +88,7 @@ function Others({ previousStep, triggerNextStep, t }) {
 
       triggerNextStep(newTrigger)
     }
-  }  
+  }
 
   if (!message) return <Loading />
 
@@ -96,7 +96,7 @@ function Others({ previousStep, triggerNextStep, t }) {
     <Fragment>
       {message}
       <div>
-        {!notGuess && continueOptions.map((item, idx) => (          
+        {!notGuess && continueOptions.map((item, idx) => (
           <Button
             key={uuid()}
             variant='outline-dark'
@@ -106,7 +106,7 @@ function Others({ previousStep, triggerNextStep, t }) {
             {t(`stepOptions:continueOptions.${idx}`)}
           </Button>
         ))}
-      </div>      
+      </div>
     </Fragment>
   )
 }
