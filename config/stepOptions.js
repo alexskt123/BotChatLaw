28bc07703de7f980e8ebc8b80d8fa0c32713dc28
So Alex <as2120@ad01.bcthk.com>
2020-10-23 11:15:14 +0800

    split stageask into en and zh to support switch lang

--- config/stepOptions.js
+++ config/stepOptions.js
@@ -21,27 +21,53 @@ export const continueOptions = [
   }
 ]
 
-export const defaultOptions = [
-  {
-    label: '結束',
-    value: '結束',
-    trigger: 'tail'
-  },
-  {
-    label: '發問',
-    value: '發問',
-    trigger: 'other'
-  }
-  ,
-  {
-    label: '有咩可以問',
-    value: '有咩可以問',
-    trigger: 'stageintent'
-  }
-  ,
-  {
-    label: '聯絡我們',
-    value: '聯絡我們',
-    trigger: 'stagecontact'
-  }
-]
+export const defaultOptions = {
+  zh: [
+    {
+      label: '結束',
+      value: '結束',
+      trigger: 'tail'
+    },
+    {
+      label: '發問',
+      value: '發問',
+      trigger: 'other'
+    }
+    ,
+    {
+      label: '有咩可以問',
+      value: '有咩可以問',
+      trigger: 'stageintent'
+    }
+    ,
+    {
+      label: '聯絡我們',
+      value: '聯絡我們',
+      trigger: 'stagecontact'
+    }
+  ],
+  en: [
+    {
+      label: 'End',
+      value: '結束',
+      trigger: 'tail'
+    },
+    {
+      label: 'Ask',
+      value: '發問',
+      trigger: 'other'
+    }
+    ,
+    {
+      label: 'What can be asked?',
+      value: '有咩可以問',
+      trigger: 'stageintent'
+    }
+    ,
+    {
+      label: 'Contact Us',
+      value: '聯絡我們',
+      trigger: 'stagecontact'
+    }
+  ]
+}
