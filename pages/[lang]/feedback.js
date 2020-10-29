8bff31deaa0bbd5d1b227ddecb6e9ded69727cc3
Li Eddy <el2083@ad01.bcthk.com>
2020-10-29 11:25:44 +0800

    use state for form value

--- pages/[lang]/feedback.js
+++ pages/[lang]/feedback.js
@@ -18,15 +18,23 @@ function Feedback({ t, i18n }) {
 
   const [config, setConfig] = useState([])
   const [validated, setValidated] = useState(false)
+  const [formValue, setFormValue] = useState({})
+
+  const handleChange = (e) => {
+    setFormValue({
+      ...formValue,
+      [e.target.name]: e.target.value
+    })
+  }
 
   const handleSubmit = async (event) => {
+    event.preventDefault()
     const form = event.currentTarget
+
     if (form.checkValidity() === false) {
-      event.preventDefault()
       event.stopPropagation()
     } else {
-      event.preventDefault()
-      await submitFeedback(event.target.elements['0'].value, event.target.elements['1'].value, event.target.elements['2'].value)
+      await submitFeedback(formValue)
       router.push('/')
     }
     setValidated(true)
@@ -46,15 +54,15 @@ function Feedback({ t, i18n }) {
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group controlId="exampleForm.ControlInput1">
               <Form.Label>{config.name}</Form.Label>
-              <Form.Control required type="name" />
+              <Form.Control required type="name" name="name" onKeyUp={(e) => handleChange(e)} />
             </Form.Group>
             <Form.Group controlId="exampleForm.ControlInput2">
               <Form.Label>{config.email}</Form.Label>
-              <Form.Control required type="email" placeholder="name@example.com" />
+              <Form.Control required type="email" name="email" placeholder="name@example.com" onKeyUp={(e) => handleChange(e)} />
             </Form.Group>
             <Form.Group controlId="exampleForm.ControlTextarea1">
               <Form.Label>{config.feedback}</Form.Label>
-              <Form.Control required as="textarea" rows={3} />
+              <Form.Control required name="feedback" as="textarea" rows={3} onKeyUp={(e) => handleChange(e)} />
             </Form.Group>
             <Button variant="primary" type="submit">
               {config.submit}
